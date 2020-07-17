f0476908d6603377a2c0e555f52ae4d73638d08b
JoeCheong23 <j.cheong@hotmail.com>
2020-07-17 19:09:43 +1200

    Added basic design for 3 questions and their answers. JS requires further development

---
+++ script/responseScript.js
@@ -0,0 +1,12 @@
+var ans;
+
+function submit(btnId) {
+    console.log("yeet")
+    alert("Yeet");
+    document.getElementById("reponse_1")
+}
+
+function submit(){
+    console.log("yellow")
+    
+}
